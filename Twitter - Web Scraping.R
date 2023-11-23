2c0d98b420732b23e92c4610b6ef2f67394a27f5
Serge Geukjian <sergegeukjian@gmail.com>
2023-11-23 16:22:50 +0200

    New Attempt
    Dropped Twitter scraping as it is not working:
    - Either 403 error: Permission not granted
    - Returning empty tibble.
    
    Started a new web scraping code where we scrape quotes from a website.

---
+++ Twitter - Web Scraping.R
@@ -0,0 +1,70 @@
+# Install and load the rtweet package
+#install.packages("rtweet")
+#install.packages("twitteR")
+library(ROAuth)
+library(rtweet)
+library(twitteR)
+library(tm)
+
+# Set up your Twitter API credentials
+api_key <-  "SG5sB2urKKZLK7XtAI9ph3E8l"
+#api_key <- "czFYWFdEUllvU2RxQlpBb05NZmU6MTpjaQ" #Client ID
+api_secret_key <- "LLYPBPYl9zDiz1lvssYW0fdt5C8F6laqTp01Dn9g85iXHmpHIG"
+#api_secret_key <- "Q2VFKtKjYZ4c7eVf7MO8CJheoxU8FdGn3_Daqma9Ulma5GcDoP" #Client Secret
+access_token <- "1622856438132383746-XoJjcJ8KmFOWjmOdyIcZKkg7bO02ZX"
+access_token_secret <- "4Zz6QpvLZIAfXJD3pEVuk6fDYoBVn2u7fjPcLn5vL0cNb"
+
+rtweet_app('AAAAAAAAAAAAAAAAAAAAACd7rAEAAAAAK9DgEOgXixzH2IShqfDfdJtYUIo%3DeUwGQ1FKgm9tQiMAlB2ApqSHbCiOqpi5B21S3HbdGpWPUgSElp')
+get_timeline('pontifex')
+
+# Set up the Twitter API connection
+#rtweet
+token <- create_token(
+  app = "Scraper_SG",
+  consumer_key = api_key,
+  consumer_secret = api_secret_key,
+  access_token = access_token,
+  access_secret = access_token_secret
+)
+
+token
+
+#twitteR
+#setup_twitter_oauth(api_key, api_secret_key, access_token, access_token_secret)
+
+t <- Sys.time()
+
+# Search for tweets
+#rtweet
+tweets <- search_tweets(
+  'weather',  # Your search query
+  n = 3,          # Number of tweets to fetch
+  include_rts = FALSE, 
+  retryonratelimit = F,
+  token = token
+)
+
+Sys.time() -t
+head(tweets)
+#twitteR
+#tweets <- searchTwitter(
+#  '@realDonaldTrump',  # Your search query
+#  n = 3,          # Number of tweets to fetch
+#  include_rts = FALSE, 
+#  lang="en", 
+#  since="2023-11-01", 
+#  until="2023-11-20"
+#)
+
+#today_trends <- getTrends(2364559)
+#tweets.df <- twListToDF(tweets)
+
+# Print the text of the first few tweets
+head(tweets$text)
+
+#bearer token = AAAAAAAAAAAAAAAAAAAAACd7rAEAAAAA%2FOT3dY9BNREkdSr1oZq9GC2R8pY%3D7jXc4YX1U2JU71ECDv4bv8fsiRSp9d2FzFtvueFHdecGbAqFyD
+#api_key = SG5sB2urKKZLK7XtAI9ph3E8l
+#api_secret_key = LLYPBPYl9zDiz1lvssYW0fdt5C8F6laqTp01Dn9g85iXHmpHIG
+#access_token <- 1622856438132383746-yGJyTPLDq70CWoABwBg1IZjhHMqpX8
+#access_token_secret <- dl5AZevdLgtpmyGym15g1XwiIS3miBP69YGandWRdl6LB
+#ProfFeynman
