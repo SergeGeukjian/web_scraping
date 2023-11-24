3e25b9b48c3da42b8863a7cae752ef9dbf00f261
Serge Geukjian <sergegeukjian@gmail.com>
2023-11-24 12:43:02 +0200

    Added backup//Read all website pages

--- Quotes - Web Scraping.R
+++ Quotes - Web Scraping.R
@@ -4,51 +4,48 @@ library(dplyr)
 library(stringr)
 
 
-
 url <- 'https://quotes.toscrape.com/'
+base_url <- 'https://quotes.toscrape.com'
+page_num <- 1
+quotes_list <- list()
 
-# Reading the html file-----------------
-page <- read_html(url)
-page
+while (TRUE) {
+  # Reading the html file
+  page <- read_html(url)
+  
+  # Retrieving the contents of the html
+  quotes <- page %>% html_nodes("div.quote")
+  
+  if (length(quotes) == 0) {
+    break  # No more pages
+  }
+  
+  # Extracting information from the current page
+  quote <- quotes %>% html_nodes("span.text") %>% html_text()
+  author <- quotes %>% html_nodes("small.author") %>% html_text()
+  author_link <- paste0(base_url, quotes %>% html_nodes("span small.author + a") %>% html_attr("href"))
+  tags <- quotes %>% html_nodes("div.tags") %>% html_text()
+  
+  # Putting the extracted data in a structured table
+  current_quotes <- data.frame(quote, author, author_link, tags, stringsAsFactors = FALSE)
+  
+  # Combine quotes from all pages
+  quotes_list <- c(quotes_list, list(current_quotes))
+  
+  # Check for the next page
+  next_page <- page_num + 1
+  url <- paste0(base_url, '/page/', next_page, '/')
+  page_num <- next_page
+}
 
-# Retrieving the contents of the html-------------
-quotes <- page %>%
-  html_nodes("div.quote")
+# Combine all quotes into one data frame
+all_quotes <- do.call(rbind, quotes_list)
 
-## Getting the quotes------------
-quote <- quotes %>%
-  html_nodes("span.text")%>%
-  html_text()
-
-## Getting the authors of the quotes------------
-author <- quotes %>%
-  html_nodes("small.author")%>%
-  html_text()
-
-## Getting the link of author-----------
-author_link <- quotes %>%
-  html_nodes("span small.author + a") %>% 
-  html_attr("href")
-
-author_link <- paste0("https://quotes.toscrape.com/",author_link)
-
-
-## Getting the tags of each quote so we can put them on different pages on R shiny-----
-tags <- quotes %>%
-  html_nodes("div.tags")%>%
-  html_text()
-
-### Add manipulation here that puts the tags as a list. That way you can later filter with what is in the list as separate pages on Shiny
-
-
-# Putting the extracted data in a structured table---------
-all_quotes <- data.frame(quote, author, author_link, tags)
-
-## Naming of columns--------
-names(all_quotes) <- c("quote", "author", "author_link", "tags")
-
+# Clean up the tags column
 all_quotes$tags <- gsub("\\s+", " ", all_quotes$tags)
-all_quotes$tags <- gsub("Tags:", " ", all_quotes$tags)
-all_quotes
+all_quotes$tags <- gsub("Tags:", "", all_quotes$tags)
 
-#write.csv(all_quotes, "C:/Users/s.geukjian/Desktop/all_quotes.csv")
+# Print the resulting data frame
+print(all_quotes)
+
+
